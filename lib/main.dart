693dd0ac243d0ed8219853bcc2873ca6a678ee75
JunHyeokDev <fixme1537@gmail.com>
2024-06-03 11:28:45 +0900

    [1] [UI] TextField and Button done.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +36,71 @@ class LogIn extends StatelessWidget {
               onPressed: (){}
           )
         ],
+      ),
+      body: SingleChildScrollView (
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50),),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
+            ),
+            Form(child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15.0
+                    )
+                  )
+                ),
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0),
+                  child: Column(
+
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "Dice"'
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Enter Password'
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      ButtonTheme(
+                        minWidth: 100.0,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all<Color>(Colors.white)
+                          ),
+                          onPressed: () {},
+                          child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.grey,
+                              size: 35,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            ),
+            )
+          ],
+        ),
       ),
     );
   }
